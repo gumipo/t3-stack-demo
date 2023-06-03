c90a0d8e9adf451aa85ad4a6130c53396dda56a5
gumipo <10sei0otto.22@gmail.com>
2023-06-03 16:14:47 +0900

    add layout

---
+++ src/components/SideNav.tsx
@@ -0,0 +1,30 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+
+const SideNav = () => {
+  const session = useSession();
+  const user = session.data?.user;
+  return (
+    <nav className="sticky top-0 px-2 py-4">
+      <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        {user && (
+          <li>
+            <Link href={`/profiles/${user.id}`}>Profile</Link>
+          </li>
+        )}
+
+        <li>
+          <button onClick={() => (!user ? signIn() : signOut())}>{`Log ${
+            !user ? "In" : "Out"
+          }`}</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default SideNav;
